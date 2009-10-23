6644593380a7709435292da17d48293145f2d00d
William Roberts <williamr@symbian.org>
2009-10-23 14:11:10 +0100

    Remove incorrect "How to build" documentation and scripts

--- omap3530/beagleboard/rom/build_beagle_touchpad.bat
+++
@@ -1,4 +0,0 @@
-
-call \epoc32\tools\buildrom_lpd.cmd beagle Symbian2.oby -D_EABI=ARMV5 -DCOLOR  -DSYMBIAN_EXCLUDE_IPSEC -D_PORTRAIT_ -es60ibymacros -nosymbols -D__LOCALES_01_IBY__ -DSYNAPTICS_TOUCH_PAD
-
-
